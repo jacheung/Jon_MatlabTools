f6dc4a16821d299edfe02cdcfdf11ca9bb2a03c9
Jon Cheung <jacheung6@gmail.com>
2019-08-22 17:06:31 -0700

    code cleanup

--- old/assist_touchmasks.m
+++ old/assist_touchmasks.m
@@ -1,4 +1,4 @@
-function [mask] = maskBuilder(array)    
+function [mask] = assist_touchmasks(array)    
 % Function used to pull out masks based on touches and pole availability
 
 % OUTPUT: 
@@ -12,34 +12,32 @@ function [mask] = maskBuilder(array)
     tmp=find(array.S_ctk(9,:,:)==1)/array.t;
     d = tmp-floor(tmp); %keep only decimals
     avail=round(min(d)*array.t); %pole available time in ms based on first touch 
-    pOnset = round(array.meta.poleOnset(1)*1000); 
     offset=round(array.meta.poleOffset*1000);
     offmax=find(offset>array.t);
     offset(offmax)=array.t;
-    meanOffset = round(mean(array.meta.poleOffset)*1000); 
-    samplingPeriod = 750;
    
     
     spikes = squeeze(array.R_ntk);
     
     licks = squeeze(array.S_ctk(16,:,:))';
-    fl = nan(1,array.k);
-    for i = 1:size(licks,1)
-        fl(i) = min([find(licks(i,pOnset+samplingPeriod:end)==1)+pOnset+samplingPeriod array.t]);
-    end
-    fl2 = fl(fl~=array.t);
-    flninety = round(prctile(fl2,90)); 
-    nonLickTrials = fl==array.t; 
-    fl(nonLickTrials)=flninety;
-
     
-    timePostTouchToTrim = 30; 
+    fl = [];
+    for i = 1:size(licks,1)
+        fl(i) = min([find(licks(i,:)==1) array.t]);
+    end
+    fl2 = fl(fl<3000);
+    fls = sort(fl2);
+    flninety= fls(ceil(numel(fls)*.9));
+    fl(fl==array.t)=flninety;
+    
+    
     firsttouchIdx = [find(array.S_ctk(9,:,:)==1)];
     firsttouchOffIdx = [find(array.S_ctk(10,:,:)==1)];
     touchOnIdx = [find(array.S_ctk(9,:,:)==1); find(array.S_ctk(12,:,:)==1)];
     touchOffIdx = [find(array.S_ctk(10,:,:)==1); find(array.S_ctk(13,:,:)==1)];
-    touchOnIdx = touchOnIdx(touchOffIdx<numel(spikes)-timePostTouchToTrim+5);
-    touchOffIdx = touchOffIdx(touchOffIdx<numel(spikes)-timePostTouchToTrim+5);
+    if touchOffIdx<(numel(spikes)-70) %ensures that offset is within index boundaries
+        touchOffIdx = touchOffIdx+70;
+    end
     
     firsttouchEx_mask = ones(size(squeeze(array.S_ctk(1,:,:))));
     for i = 1:length(firsttouchIdx)
@@ -48,15 +46,16 @@ function [mask] = maskBuilder(array)
     
     touchEx_mask = ones(size(squeeze(array.S_ctk(1,:,:))));
     for i = 1:length(touchOnIdx)
-        touchEx_mask(touchOnIdx(i):touchOffIdx(i)+timePostTouchToTrim) = NaN; %added 30 to add time from touch offset 
+        touchEx_mask(touchOnIdx(i):touchOffIdx(i)) = NaN;
     end
     touchEx_mask(1:100,:) = 1; %since bleedover from end of trials before, tihs ensure we keep end
-
+    
+%     availtotrialend_mask=ones(size(squeeze(array.S_ctk(1,:,:))));
+%     availtotrialend_mask(avail:size(availtotrialend_mask,1),:) = NaN; %block out all periods after pole availability
+%     
     avail_mask=ones(size(squeeze(array.S_ctk(1,:,:))));
-    avail_meanOffset = NaN(size(squeeze(array.S_ctk(1,:,:))));
     for j=1:array.k
         avail_mask(avail:offset(j),j)=NaN; %block out all periods between pole availability
-        avail_meanOffset(pOnset:meanOffset,j) = 1; 
     end
     
     sampling_mask=NaN(size(squeeze(array.S_ctk(1,:,:))));
@@ -66,33 +65,20 @@ function [mask] = maskBuilder(array)
     end
    
     availToFirstlick = NaN(size(squeeze(array.S_ctk(1,:,:))));
-    pOnsetToFirstLick = NaN(size(squeeze(array.S_ctk(1,:,:))));
-    pOnsetToMedianFirstLick = NaN(size(squeeze(array.S_ctk(1,:,:))));
     availToEnd = availToFirstlick  ;
     for d = 1:array.k
        availToFirstlick(round(((array.meta.poleOnset(1))*1000),0):fl(d),d)=1;
        availToEnd(round(((array.meta.poleOnset(1))*1000),0):end,d)=1;
-       pOnsetToFirstLick(pOnset:fl(d) , d) = 1; 
-       pOnsetToMedianFirstLick(pOnset:flninety, d) = 1; 
     end
     
-    amplitude = squeeze(array.S_ctk(3,:,:)); 
-    whisking = nan(size(squeeze(array.S_ctk(1,:,:))));
-    whisking(amplitude>5)=1;
     
-    quiet = nan(size(squeeze(array.S_ctk(1,:,:))));
-    quiet(amplitude<5)=1;
     
-    mask.nonlickTrials = nonLickTrials; 
-    mask.onsettolick = pOnsetToFirstLick;
-    mask.onsettoMedianlick = pOnsetToMedianFirstLick;
+    
+    
     mask.availtolick = availToFirstlick;
-    mask.availtoMeanOffset = avail_meanOffset; 
     mask.touch = touchEx_mask;
     mask.first = firsttouchEx_mask;
     mask.availend = availToEnd;
     mask.avail = avail_mask;
     mask.samplingp = sampling_mask;
-    mask.whisking = whisking;
-    mask.quiet = quiet;
-    
+    mask.samp_notouch = (touchEx_mask.*sampling_mask);
